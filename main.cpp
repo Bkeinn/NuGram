1151c115a36fbb15e7e36fb7288d29883f63f7c8
heimchen <e12330570@student.tuwien.ac.at>
2025-05-31 22:24:03 +0200

    Made to work with new outputformat

--- main.cpp
+++ main.cpp
@@ -9,24 +9,35 @@ const int min_ascii = 32;
 const int max_ascii = 126;
 const int width = 128 - min_ascii;
 const int deapth = 5;
+const int output_deapth = 253*2;
+
+struct Data {
+  std::string data;
+  long long count;
+  long long value;
+
+  Data(std::string d, long long c) : data(d), count(c), value((d.size() - 1) * c) {};
+};
+
+std::vector<Data> saver;
 
 struct Tree {
   int passes;
   std::array<Tree*, 256> next;
 
-  Tree() : passes(0) {};
+  Tree() : passes(0), next{} {};
 };
 
 Tree* add(Tree* root, char character) {
-  if (root->next[character - min_ascii] == nullptr) {
-    root->next[character - min_ascii] = new Tree();
+  if (root->next[character] == nullptr) {
+    root->next[character] = new Tree();
     // std::cout << character << std::endl;
   }
-  root->next[character - min_ascii]->passes++;
-  return root->next[character - min_ascii];
+  root->next[character]->passes++;
+  return root->next[character];
 }
 
-void adder(std::array<char, deapth> vec, Tree* root){
+void adder(std::array<char, deapth>& vec, Tree* root){
   if (vec[0] == 0){
     return;
   }
@@ -37,23 +48,53 @@ void adder(std::array<char, deapth> vec, Tree* root){
   }
 }
 
-void printtree(Tree* root, std::string s){
-  std::cout << s << "'," << root->passes << "," << (s.size() - 2) * root->passes << ",\n";
-  for(int i = 0; i < root->next.size() - 1; i++){
-    if(root->next[i] != nullptr){
-      printtree(root->next[i], s + char(i));
+void print_tree(Tree* node, const std::string& s) {
+  // s starts with one leading apostrophe
+  std::cout << s << "','" << node->passes
+            << ',' << (s.size()-1)*node->passes 
+            << "\n";
+  for (int i = min_ascii; i <= max_ascii; ++i) {
+    if (node->next[i] != nullptr) {
+      print_tree(node->next[i], s + char(i));
     }
   }
 }
 
+
+void add_to_saver(Tree* root, std::string s){
+  saver.emplace_back(Data(s, root->passes));
+  for (int i = min_ascii; i <= max_ascii; ++i) {
+    if (root->next[i] != nullptr) {
+      add_to_saver(root->next[i], s + char(i));
+    }
+  }
+}
+
+void saver_exporter() {
+  struct {
+    bool operator() (const Data a, const Data b) const {
+      return a.value > b.value;
+    }
+  } custom;
+  std::sort(saver.begin(), saver.end(), custom);
+  // for(const Data& d : saver){
+  //   std::cout << '\'' << d.data << "'," << d.count << ',' << d.value << '\n';
+  // }
+  for(int i = 0;i < output_deapth - 1; i++) {
+    std::cout << saver[i].data << ',';
+  }
+  std::cout << saver[output_deapth - 1].data << std::endl;
+}
+
 int main(){
   char ch;
-  std::array<char,deapth> context;
+  std::array<char,deapth> context{};
+  //std::freopen("/home/heimchen/Documents/Personal/MolRecap.typ", "r", stdin);
 
   Tree* ngram = new Tree();
 
   while (std::cin.get(ch)){
-      if(int(ch) >= min_ascii && int(ch) <= max_ascii){
+      if(int(ch) >= min_ascii && int(ch) <= max_ascii && char(ch) != '\'' && char(ch) != ','){ // this caps out ' and , so the csv does not get destroyed
         std::rotate(context.begin(), context.begin() + 1, context.end());
         context[deapth - 1] = ch;
         adder(context, ngram);
@@ -63,7 +104,16 @@ int main(){
       }
   }
 
-  printtree(ngram, "'");
+  for (int i = min_ascii; i <= max_ascii; ++i) {
+    if (ngram->next[i]) {
+      //std::cout << ngram->next[i]->passes << "\n";
+      //print_tree(ngram->next[i], "'" + char(i));
+      std::string s;
+      add_to_saver(ngram->next[i], s.assign(1,char(i)));
+    }
+  }
+
+  saver_exporter();
   
   return 0;
 }
